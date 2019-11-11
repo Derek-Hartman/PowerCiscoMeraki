2b3fa4884c29765e5c5a297ea3fbcfb2b8315871
Derek-Hartman <57258132+Derek-Hartman@users.noreply.github.com>
2019-11-11 16:37:30 -0500

    Added Delete Functions

---
+++ 1.0.0/Public/Delete-MerakiAPIKey.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.Synopsis
+    Deleted the encrypted Key
+
+.EXAMPLE
+    Delete-MerakiAPIKey
+
+.NOTES
+   Modified by: Derek Hartman
+   Date: 11/11/2019
+
+#>
+
+function Delete-MerakiAPIKey {
+    Remove-ItemProperty -Path "HKCU:\Software\PowerCiscoMeraki" -Name "APIKey"    
+}
---
+++ 1.0.0/Public/Delete-MerakiOrgID.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.Synopsis
+    Deleted the encrypted OrgID
+
+.EXAMPLE
+    Delete-MerakiOrgID
+
+.NOTES
+   Modified by: Derek Hartman
+   Date: 11/11/2019
+
+#>
+
+function Delete-MerakiOrgID {
+    Remove-ItemProperty -Path "HKCU:\Software\PowerCiscoMeraki" -Name "OrgID"    
+}
