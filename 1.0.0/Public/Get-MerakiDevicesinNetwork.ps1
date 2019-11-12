1d058aec07febc8bf1a4208a0d380b2eff163922
Derek-Hartman <57258132+Derek-Hartman@users.noreply.github.com>
2019-11-12 15:59:23 -0500

    Added get-deviceinnetwork

---
+++ 1.0.0/Public/Get-MerakiDevicesinNetwork.ps1
@@ -0,0 +1,80 @@
+﻿<#
+.SYNOPSIS
+    Returns Devices in a specific network
+
+.Example
+    Get-MerakiDevicesinNetwork -NetworkID "NID"
+    Get-MerakiDevicesinNetwork -ApiKey "key" -NetworkID "NID"
+
+.NOTES
+    Modified by: Derek Hartman
+    Date: 11/12/2019
+#>
+
+Function Get-MerakiDevicesinNetwork {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory = $False,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your API Key.")]
+        [Alias('API')]
+        [string[]]$ApiKey,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Network ID.")]
+        [Alias('NetID')]
+        [string[]]$NetworkID
+    )
+
+    $RegistryKeyPath = "HKCU:\Software\PowerCiscoMeraki"
+    $Key = Get-ItemProperty -Path $RegistryKeyPath
+
+    If ([string]::IsNullOrEmpty($key.APIKey)) {
+    }
+    Else {
+        $securestring = convertto-securestring -string ($key.APIKey)
+        $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securestring)
+        $ak = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+        $ApiKey = $ak
+    }
+
+    If ([string]::IsNullOrEmpty($ApiKey)) {
+        Write-Host "-APIKey needs to be given or set running Set-MerakiAPIKey function" -ForegroundColor Yellow -BackgroundColor Red
+    }
+    Else {
+
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+        $Uri = @{
+            "Network" = "https://api.meraki.com/api/v0/networks/$NetworkID/devices"
+        }
+
+        $Rest = Invoke-RestMethod -Method GET -Uri $Uri.Network -Headers @{
+            'X-Cisco-Meraki-API-Key' = "$ApiKey"
+            'Content-Type'           = 'application/json'
+        }
+
+        foreach ( $item in $Rest ) {
+            $Settings = $item | Select-Object -Property *
+            $l3FirewallRulesProperties = @{
+                lat             = $Settings.lat
+                lng             = $Settings.lng
+                address         = $Settings.address
+                serial          = $Settings.serial
+                mac             = $Settings.mac
+                lanIp           = $Settings.lanIp
+                networkId       = $Settings.networkId
+                model           = $Settings.model
+                switchProfileId = $Settings.switchProfileId
+                firmware        = $Settings.firmware
+                floorPlanId     = $Settings.floorPlanId
+                wan1Ip          = $Settings.wan1Ip
+                wan2Ip          = $Settings.wan2Ip
+            }
+            $obj = New-Object -TypeName PSObject -Property $l3FirewallRulesProperties
+            Write-Output $obj
+        }
+    }
+}
