469ede413a82ea3a26a4b30a977d950bd9db7f2f
Derek-Hartman <57258132+Derek-Hartman@users.noreply.github.com>
2021-04-16 14:44:19 -0400

    Update Set-MerakiMXL3FirewallRules.ps1

--- 1.0.0/Public/Set-MerakiMXL3FirewallRules.ps1
+++ 1.0.0/Public/Set-MerakiMXL3FirewallRules.ps1
@@ -1,13 +1,13 @@
 ﻿<#
 .SYNOPSIS
-    Update the L3 firewall rules of an MX network
+    Update the L3 firewall rules of an MX network.
+    Requires passing a json with all rules to apply. 
 
 .Example
-    Set-MerakiMXL3FirewallRules -NetworkID "NID"
-    Set-MerakiMXL3FirewallRules -ApiKey "key" -NetworkID "NID"
+    Set-MerakiMXL3FirewallRules -NetworkID "NID" -Json $JSON
+    Set-MerakiMXL3FirewallRules -ApiKey "key" -NetworkID "NID" -Json $JSON
 
 .NOTES
-    !!!!!Not Ready will wipe out all run except 1!!!!
     Modified by: Derek Hartman
     Date: 11/12/2019
 #>
@@ -26,7 +26,12 @@ Function Set-MerakiMXL3FirewallRules {
             ValueFromPipeline = $True,
             HelpMessage = "Enter your Network ID.")]
         [Alias('NetID')]
-        [string[]]$NetworkID
+        [string[]]$NetworkID,
+
+         [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Rules JSON.")]
+        [string[]]$JSON
     )
 
     $RegistryKeyPath = "HKCU:\Software\PowerCiscoMeraki"
@@ -51,20 +56,8 @@ Function Set-MerakiMXL3FirewallRules {
         $Uri = @{
             "l3FirewallRules" = "https://api.meraki.com/api/v1/networks/$NetworkID/appliance/firewall/l3FirewallRules"
         }
-        $RuleConfig = @{"comment" = "PS Rule Test2"}
-        $RuleConfig += @{"policy" = "allow"}
-        $RuleConfig += @{"protocol" = "tcp"}
-        $RuleConfig += @{"destPort" = "447"}
-        $RuleConfig += @{"destCidr" = "192.168.1.0/24"}
-        $RuleConfig += @{"srcPort" = "Any"}
-        $RuleConfig += @{"srcCidr" = "Any"}
-        $RuleConfig += @{"syslogEnabled" = "false"}
-        $Body = @{"rules" = @($RuleConfig)}
 
-
-        $postData = ConvertTo-Json $Body
-
-        $Rest = Invoke-RestMethod -Method PUT -Uri $Uri.l3FirewallRules -Body $postData -Headers @{
+        $Rest = Invoke-RestMethod -Method PUT -Uri $Uri.l3FirewallRules -Body $JSON -Headers @{
             'X-Cisco-Meraki-API-Key' = "$ApiKey"
             'Content-Type'           = 'application/json'
         }
